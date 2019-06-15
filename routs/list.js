c4feefec13da051ae8348d5efd6d030926af4f92
romanb123 <44100462+romanb123@users.noreply.github.com>
2019-06-15 21:30:24 +0300

    rty

--- routs/list.js
+++ routs/list.js
@@ -1,12 +1,12 @@
 var express = require('express');
 const router = express.Router();
-const products=require('./add-product');
+const products = require('./add-product');
 const path = require('path');
 
 
 router.get('/list', function (req, res) {
-  res.render('list');
-  console.log("from list",products.products);
+  res.render('list', { lists: products.products });
+  console.log("from list", products.products);
 });
 
-module.exports=router;
+module.exports = router;
