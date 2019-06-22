4c6fc86c4ea99a17fd907c7de9044d32f56e4e85
romanb123 <44100462+romanb123@users.noreply.github.com>
2019-06-22 19:22:36 +0300

    upfff

--- modules/prodcts.js
+++ modules/prodcts.js
@@ -1,4 +1,5 @@
-allproducts = [];
+var fs = require('fs');
+var path = require('path');
 
 module.exports = class Product {
     constructor(name) {
@@ -6,7 +7,19 @@ module.exports = class Product {
     }
 
     addToAll() {
-        allproducts.push(this);
+        var datapath = path.join("data", "data.json");
+
+        fs.readFile(datapath, (err, data) => {
+            let allproducts = [];
+            if (!err) {
+                allproducts = JSON.parse(data);
+            }
+            allproducts.push(this);
+            fs.writeFile(datapath, JSON.stringify(allproducts), (err) => {
+                console.log(err);
+            });
+        });
+        console.log(allproducts);
     }
     static showProducts() {
         return allproducts;
